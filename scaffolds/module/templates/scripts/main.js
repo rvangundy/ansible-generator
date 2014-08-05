85a31e45ad076d8e831ff75c36265b7ced13e773
Ryan VanGundy <ryan.vangundy@gmail.com>
2014-08-05 13:27:23 -0400

    Begin working on module scaffolding

---
+++ scaffolds/module/templates/scripts/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// {{ module_name }}
+
+/**
+ * Description
+ * @param  {DocumentFragment} fragment
+ * @param  {Model} model A backbone-orm model
+ */
+module.exports = function (fragment, model) {
+
+};
